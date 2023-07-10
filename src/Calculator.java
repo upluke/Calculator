ab4b79d22fa6b635abd086dd8b639af1b502826c
upluke <lukeforadream@gmail.com>
2023-07-10 00:07:21 -0700

    added minus mult divid cancel

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,10 +7,14 @@ import java.awt.event.KeyEvent;
 public class Calculator extends JFrame  {
     private String[] keyValues ={"7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", "c", "0", "=", "/"} ;
     private String operators = "+-*/=c";
+    private String curOperator;
     private int result = 0;
     private int tempNum= 0;
     private boolean isFirstTyping = true;
     private boolean isEmptyField = false;
+
+    private boolean isAfterEqualOpration= false;
+    private boolean isFirstMinus = true;
 
     public Calculator(){
         super("Calculator");
@@ -61,7 +65,7 @@ public class Calculator extends JFrame  {
                 if(!operators.contains(buttonText)){
 
                     if(isFirstTyping | isEmptyField){
-                        System.out.println("state*: "+ buttonText + "result: " + result);
+                        System.out.println("entry result: " + result);
                         textField.setText("");
                         isEmptyField=false;
                         isFirstTyping=false;
@@ -69,7 +73,7 @@ public class Calculator extends JFrame  {
 
                     textField.setText(textField.getText() + buttonText);
                     tempNum= Integer.parseInt(textField.getText());
-                    System.out.println("tempNum: "+ tempNum);
+                    System.out.println("startstart: result"+result +"tempNum: "+ tempNum);
                     isEmptyField=false;
                 }else{
                     switch (buttonText){
@@ -81,20 +85,80 @@ public class Calculator extends JFrame  {
                             if(result!=Integer.parseInt(textField.getText())){
                                 textField.setText(Integer.toString(result));
                             }
-
+                            curOperator="+";
                             break;
                         case "-":
-                            // TODO: Fix this
-//                            System.out.println(textField.getText());
-//                            result-= Integer.parseInt(textField.getText());
-//                            isEmptyField=true;
-//                            System.out.println("result in -: "+result);
-//                            // text field will update with the result after 1st round of operation
-//                            if(result!=Integer.parseInt(textField.getText())){
-//                                textField.setText(Integer.toString(result));
-//                            }
-//                            break;
+                            if(isFirstMinus | isAfterEqualOpration){
+                                result=Math.abs(tempNum-result);
+                                isAfterEqualOpration=false;
+                                isFirstMinus=false;
+                            }else{
+                                result-= tempNum;
+                            }
 
+                            isEmptyField=true;
+                            System.out.println("result in -: "+result);
+                            if(result!=Integer.parseInt(textField.getText())){
+                                textField.setText(Integer.toString(result));
+                            }
+                            curOperator="-";
+                            break;
+                        case "*":
+                            System.out.println("*result: "+result + " tempNum: "+tempNum);
+                            if(result ==0){
+                                result=1;
+                            }
+                            result*= tempNum;
+                            isEmptyField=true;
+                            System.out.println("result in *: "+result);
+                            if(result!=Integer.parseInt(textField.getText())){
+                                textField.setText(Integer.toString(result));
+                            }
+                            curOperator="*";
+                            break;
+                        case "/":
+                            System.out.println("/result: "+result + " tempNum: "+tempNum);
+                            if(result ==0){
+                                result = tempNum;
+                            }else{
+                                result/= tempNum;
+                            }
+
+                            isEmptyField=true;
+                            System.out.println("result in /: "+result);
+                            if(result!=Integer.parseInt(textField.getText())){
+                                textField.setText(Integer.toString(result));
+                            }
+                            curOperator="/";
+                            break;
+                        case "=":
+                            result = calculateResult(result, tempNum, curOperator);
+
+                            isEmptyField=true;
+                            isAfterEqualOpration=true;
+                            textField.setText(Integer.toString(result));
+                            if(curOperator =="*" | curOperator =="/"){
+                                tempNum=1;
+                            }else{
+                                tempNum=0;
+                            }
+                            curOperator="";
+
+                            System.out.println("in = result: "+result + " tempNum: "+ tempNum);
+
+                            break;
+                        case "c":
+                            result=0;
+                            tempNum=0;
+                            isFirstTyping=true;
+                            isEmptyField=false;
+                            textField.setText("0");
+                            curOperator="";
+                            System.out.println("------------------");
+                            System.out.println("reset1->result: "+result + " tempNum: "+tempNum);
+                            System.out.println("reset2->isFirstTyping: "+isFirstTyping + " isEmptyField: "+isEmptyField);
+                            System.out.println("Current opearator: " + curOperator);
+                            System.out.println("------------------");
                     }
 
                 }
@@ -119,6 +183,23 @@ public class Calculator extends JFrame  {
 
     }
 
+    public static int calculateResult(int result, int tempNum, String operator){
+        switch (operator) {
+            case "+":
+                result+=tempNum;
+                return result;
+            case "-":
+                result-=tempNum;
+                return result;
+            case "*":
+                result *= tempNum;
+                return result;
+            case "/":
+                result /= tempNum;
+                return result;
+        }
+        return 0;
+    }
 
 
 }
